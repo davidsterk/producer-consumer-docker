7a2b726f3d2c82430f29251c531c79340715e68d
davidsterk <46927950+davidsterk@users.noreply.github.com>
2021-09-05 15:49:42 -0400

    producer and consumer waits until connection is established with rabbitmq

--- consumer-docker/src/main/java/Main.java
+++ consumer-docker/src/main/java/Main.java
@@ -5,36 +5,65 @@ import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import service.StorageService;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class Main {
 
     private static final String RABBITMQ_HOST = System.getenv("RABBITMQ_HOST");
     private static final String QUEUE_NAME = "smartwatch";
+    private static Connection connection;
+    private static Channel channel;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException, InterruptedException, TimeoutException {
 
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost(RABBITMQ_HOST);
 
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
-        channel.queueDeclare(QUEUE_NAME, true, false, false,null);
-        System.out.println("Listening for Messages...")
-        JSONParser parser = new JSONParser();
-        DeliverCallback deliverCallback = (consumerTag, message) -> {
-            try {
-                StorageService.processData((JSONObject) parser.parse(new String(message.getBody(), "UTF-8")));;
-                System.out.println("Inserted Message");
-                channel.basicAck(message.getEnvelope().getDeliveryTag(), false);
-            } catch (Exception e) {
-                e.printStackTrace();
-                System.exit(1);
-            }
-        };
-        channel.basicConsume(QUEUE_NAME, false, deliverCallback, consumerTag -> { });
+        channel = getChannel(factory);
+        try {
+            channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+            System.out.println("Listening for Messages...");
+            JSONParser parser = new JSONParser();
+            DeliverCallback deliverCallback = (consumerTag, message) -> {
+                try {
+                    StorageService.processData((JSONObject) parser.parse(new String(message.getBody(), StandardCharsets.UTF_8)));
+                    System.out.println("Inserted Message");
+                    channel.basicAck(message.getEnvelope().getDeliveryTag(), false);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+            };
+            channel.basicConsume(QUEUE_NAME, false, deliverCallback, consumerTag -> {
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
+
+    public static Channel getChannel(ConnectionFactory factory) throws InterruptedException, IOException, TimeoutException {
+        int attempts = 0;
+        int maxAttempts = 5;
+        while(true) {
+            try {
+                connection = factory.newConnection();
+                channel = connection.createChannel();
+                return channel;
+            } catch (Exception e) {
+                System.out.println("Failed to Connect to RabbitMQ Host: "+RABBITMQ_HOST);
+                if(++attempts<maxAttempts) {
+                    System.out.println("Retrying Connection in 5 seconds...");
+                    TimeUnit.SECONDS.sleep(5);
+                } else {
+                    throw e;
+                }
+            }
+        }
+    }
 }
--- producer-docker/src/main/java/Main.java
+++ producer-docker/src/main/java/Main.java
@@ -1,3 +1,6 @@
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
@@ -6,33 +9,29 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.Channel;
 
 public class Main {
     private static final String RABBITMQ_HOST = System.getenv("RABBITMQ_HOST");
     private static final String QUEUE_NAME = "smartwatch";
-    public static void main(String[] args) {
+    private static Channel channel;
+    private static Connection connection;
+    public static void main(String[] args) throws InterruptedException, IOException, TimeoutException {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost(RABBITMQ_HOST);
-
+        Channel channel = getChannel(factory);
         try {
-            Connection connection = factory.newConnection();
-            Channel channel = connection.createChannel();
-            channel.queueDeclare(QUEUE_NAME, true, false, false,null);
-
+            channel.queueDeclare(QUEUE_NAME, true, false, false, null);
             JSONParser parser = new JSONParser();
             File inputFile = new File("input//input.txt");
             BufferedReader br = new BufferedReader(new FileReader(inputFile));
             String output;
-            while((output = br.readLine()) != null) {
+            while ((output = br.readLine()) != null) {
                 output = output.trim();
                 try {
-                    if(!output.equals("")){
-                        JSONObject json  = (JSONObject) parser.parse(output);
+                    if (!output.equals("")) {
+                        JSONObject json = (JSONObject) parser.parse(output);
                         String sensorType = json.get("sensor_name").toString().toLowerCase();
                         JSONObject message = new JSONObject();
                         message.put("type", sensorType);
@@ -50,6 +49,26 @@ public class Main {
             connection.close();
         } catch (IOException | TimeoutException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static Channel getChannel(ConnectionFactory factory) throws InterruptedException, IOException, TimeoutException {
+        int attempts = 0;
+        int maxAttempts = 5;
+        while(true) {
+            try {
+                connection = factory.newConnection();
+                channel = connection.createChannel();
+                return channel;
+            } catch (Exception e) {
+                System.out.println("Failed to Connect to RabbitMQ Host: "+RABBITMQ_HOST);
+                if(++attempts<maxAttempts) {
+                    System.out.println("Retrying Connection in 5 seconds...");
+                    TimeUnit.SECONDS.sleep(5);
+                } else {
+                    throw e;
+                }
+            }
         }
     }
 }
