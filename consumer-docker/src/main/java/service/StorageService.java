1fa0d16745b5f50dfdf4cb820396f4acab07cf62
davidsterk <46927950+davidsterk@users.noreply.github.com>
2022-01-11 16:18:42 -0500

    moved domain object creation to a domain factory static class

--- consumer-docker/src/main/java/service/StorageService.java
+++ consumer-docker/src/main/java/service/StorageService.java
@@ -12,7 +12,6 @@ import org.slf4j.LoggerFactory;
 public class StorageService {
 
     private static Logger logger = LoggerFactory.getLogger(StorageService.class);
-
     private static final DaoFactory daoFactory;
 
     private StorageService(){}
@@ -31,40 +30,10 @@ Initialize daoFactory
      */
     public static void processData(JSONObject json) throws Exception {
         SensorType sensorType = SensorType.getSensorType(json.get("type").toString());
-        Dao dao = daoFactory.getDao(sensorType);
         JSONObject contents = (JSONObject) json.get("contents");
-        Object domain;
-        switch(sensorType) {
-            case ACTIVITY:
-                domain = new Activity(contents);
-                domain = (Activity) domain;
-                break;
-            case ACTIVFIT:
-                domain = new ActivFit(contents);
-                domain = (ActivFit) domain;
-                break;
-            case HEART_RATE:
-                domain = new HeartRate(contents);
-                domain = (HeartRate) domain;
-                break;
-            case BATTERY:
-                domain = new Battery(contents);
-                domain = (Battery) domain;
-                break;
-            case BLUETOOTH:
-                domain = new Bluetooth(contents);
-                domain = (Bluetooth) domain;
-                break;
-            case LIGHT:
-                domain = new Light(contents);
-                domain = (Light) domain;
-                break;
-            default:
-                domain = new NullDomain();
-                break;
-        }
-        dao.create(domain);
-        logger.info(domain.getClass().getSimpleName() + " object inserted into msyql");
+        Dao dao = daoFactory.getDao(sensorType);
+        dao.create(DomainFactory.createModel(sensorType, contents));
+        logger.info(sensorType.getType() + " object inserted into msyql");
 
     }
 }
